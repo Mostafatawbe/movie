ee33532c6acdb45f73eefb0f4826974629900e4a
mostafa tawbe <mostafatawbe17@gmail.com>
2026-08-08 17:50:21 +0300

    Delete computershop directory

--- computershop/add_categories.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `products` (`pname`, `category`, `description`, `price`, `qtyavail`, `img`, `brand`) VALUES
-('Sample Mouse Pad', 'mouse pad', 'A sample mouse pad', 10, 100, 'mousepad_sample.jpg', 'Generic'),
-('Sample Headset', 'headset', 'A sample headset', 50, 50, 'headset_sample.jpg', 'Generic'),
-('Sample Gaming Chair', 'gaming chair', 'A sample gaming chair', 200, 20, 'chair_sample.jpg', 'Generic'),
-('Sample Table', 'table', 'A sample table', 150, 30, 'table_sample.jpg', 'Generic');
